1eb3a7b2d265200d1d4294d22bd63fcfa14f3e71
Tyoma Kazakov <tyoma@kzkv.ru>
2015-07-10 21:54:32 +0300

    initial upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    // $("#google-front-form").submit(function(e) {
+    //     e.preventDefault();
+    //     var data = {
+    //         "name1": $("[name=firstName]").val(),
+    //         "name2": $("[name=secondName]").val(),
+    //         "name3": $("[name=email]").val(),
+    //         "name4": $("[name=phone]").val(),
+    //         "name5": $("[name=check1]").is(":checked"),
+    //         "name6": $("[name=check2]").is(":checked")
+    //     };
+    //     console.log(data);
+    // });
+});
